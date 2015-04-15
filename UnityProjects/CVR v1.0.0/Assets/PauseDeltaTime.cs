3bc2796bec29cffb1e0f173b4811f1404fb0758a
fueltherage <wesleyallard1990@gmail.com>
2015-04-15 12:33:21 -0400

    OVR

---
+++ UnityProjects/CVR v1.0.0/Assets/PauseDeltaTime.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseDeltaTime : MonoBehaviour {
+
+	public float PauseTime = 10.0f;
+	public bool paused=false;
+	float pauseStartTime=0;
+	float pauseEnd;
+	// Use this for initialization
+
+	void Start () {
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			Pause();
+		}
+	}
+	public void Pause()
+	{
+		if(!paused)
+			StartCoroutine(PauseForTime());
+		else pauseEnd += PauseTime;
+
+	}
+	IEnumerator PauseForTime()
+	{
+		pauseStartTime = Time.realtimeSinceStartup;
+		pauseEnd = pauseStartTime + PauseTime;
+
+		paused = true;
+		new WaitForSeconds(PauseTime);
+		Time.timeScale = 1;
+		paused = false;
+
+		yield return null;
+	}
+}
